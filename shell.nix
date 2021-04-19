e502defe6db481133a78d869bf7cab3dcac62a6d
Matteo Joliveau <matteojoliveau@gmail.com>
2021-04-19 16:14:04 +0200

    Remove kubebuilder
    Signed-off-by: Matteo Joliveau <matteojoliveau@gmail.com>

--- shell.nix
+++ shell.nix
@@ -1,15 +1,13 @@
 let
   pkgs = import <nixpkgs> {};
   unstable = import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz) { };
-  kubebuilder = pkgs.callPackage .nix/kubebuilder.nix {};
   operator-sdk = pkgs.callPackage .nix/operator-sdk.nix {};
 in
 pkgs.mkShell {
     buildInputs = with pkgs; [
       go
-      kubebuilder
       operator-sdk
-#      unstable.kind
+      unstable.kind
       kubectl
       kustomize
     ];
